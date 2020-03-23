695f8b0da2dc9ffa7a234cc0a80851e511347b17
Mary West <westmarye1219@gmail.com>
2020-03-23 18:30:58 -0500

    console error read working on login

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import pasture from '../../components/pasture/pasture';
+
 const authDiv = $('#auth');
 const pastureDiv = $('#pasture');
 const logoutButton = $('#navbar-logout-button');
@@ -17,6 +19,7 @@ const checkLoginStatus = () => {
       authDiv.removeClass('hide');
       pastureDiv.addClass('hide');
       logoutButton.addClass('hide');
+      pasture.buildCows();
     }
   });
 };
